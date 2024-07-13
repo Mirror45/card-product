ec658fbdd433c3f98ab939989be93c5309b9d6c9
Mirror <yllikano@gmail.com>
2024-07-14 01:36:26 +0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import { createRoot } from "react-dom/client";
+import ProductPage from "./product-page";
+
+const rootElement = createRoot(document.getElementById("root"));
+rootElement.render(<ProductPage />);
