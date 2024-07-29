cea7149f92c5fb1e67403647b5ec419b1483bc1a
Mirror <yllikano@gmail.com>
2024-07-29 16:35:33 +0300

    Стили для product-page

--- src/elements/image.js
+++ src/elements/image.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 const Image = styled.img`
   max-width: 100%;
   height: auto;
+  max-width: ${(props) => props.maxWidth}px;
 `;
 
 export default Image;
--- src/product-page/product-page.jsx
+++ src/product-page/product-page.jsx
@@ -1,40 +1,58 @@
 import React from "react";
 import Title from "../title/title";
 import Code from "../code/code";
-import Gallery from "../gallery/gallery";
-import FullPrice from "../full-price/full-price";
-import Counter from "../counter/counter";
 import Description from "../description/description";
 import Comments from "../comments/comments";
 import Popularity from "../popularity/popularity";
 import Button from "/src/button/button";
+import { Image } from "/src/elements";
+import {
+  StyledProductPage,
+  Header,
+  ProductWrapper,
+  ProductInfo,
+  ProductInfoLine,
+  PageCounter,
+  BuyButton,
+  PageFullPrice,
+  DeliveryValue
+} from "./styled";
 
 
 function ProductPage({ product }) {
   return (
-    <section>
-      <Title>{product.name}</Title>
-      <Code>{product.code}</Code>
-      <div style={{ display: "flex" }}>
-        <Gallery src={product.src} alt={product.name} />
-        <div>
-          <p>
+    <StyledProductPage>
+      <Header>
+        <Title>{product.name}</Title>
+        <Code>{product.code}</Code>
+      </Header>
+      <ProductWrapper>
+        <Image
+            width="200"
+            height="257"
+            maxWidth="200"
+            src={product.src}
+            alt={product.name}
+          />
+        <ProductInfo>
+          <ProductInfoLine>
             Цена:{" "}
-            <FullPrice oldPrice={product.oldPrice} price={product.price} />
-          </p>
-          <div>
-            Количество: <Counter />
-          </div>
-          <p>
-            <span>Доставка:</span> {product.delivery}
-          </p>
-          <Button size="large">Купить</Button>
+            <PageFullPrice oldPrice={product.oldPrice} price={product.price} />
+          </ProductInfoLine>
+          <ProductInfoLine>
+            Количество: <PageCounter />
+          </ProductInfoLine>
+          <ProductInfoLine>
+            <span>Доставка:</span>{" "}
+            <DeliveryValue>{product.delivery}</DeliveryValue>
+          </ProductInfoLine>
+          <BuyButton size="large">Купить</BuyButton>
           <Popularity count={product.comments.length} />
-        </div>
-      </div>
+        </ProductInfo>
+      </ProductWrapper>
       <Description text={product.description} />
       <Comments comments={product.comments} />
-    </section>
+    </StyledProductPage>
   );
 }
 
